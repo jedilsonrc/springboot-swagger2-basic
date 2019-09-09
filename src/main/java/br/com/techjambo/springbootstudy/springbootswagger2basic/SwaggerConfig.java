c747d0466f1d5fd9b7fd1c3f3afb4048891bd57c
edilson.costa <edilson.costa@dataprev.gov.br>
2019-09-09 07:54:48 -0300

    Fixes

--- src/main/java/br/com/techjambo/springbootstudy/springbootswagger2basic/SwaggerConfig.java
+++ src/main/java/br/com/techjambo/springbootstudy/springbootswagger2basic/SwaggerConfig.java
@@ -1,8 +1,13 @@
 package br.com.techjambo.springbootstudy.springbootswagger2basic;
 
+import java.util.ArrayList;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,8 +16,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 	// Access in http://localhost:8080/swagger-ui.html
+
+	public static final Contact DEFAULT_CONTACT = new Contact("Edilson Costa", "https://techjambo.com.br", "techjambo@gmail.com");
+	public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Api Documentation - Swagger Example", "Api Documentation", "1.0",
+			"urn:tos", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0",
+			new ArrayList<VendorExtension>());
+
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2);
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO);
 	}
 }
